29b8e29d94efc6096df446541837be35c8b65cf9
surachatr <surachatr@pttep.com>
2022-06-30 08:07:45 +0700

    revise nanorc, tmux_spawner

--- tmux_spawner/tmux_run.sh
+++ tmux_spawner/tmux_run.sh
@@ -13,22 +13,24 @@ then
         exit 0 
 fi
 
-for todo_word in `ls ~/*.todo | awk -F'.' '{print $1}'`
+for todo_word in `ls ~/*.todo | awk -F'/' '{print $1}' | awk -F'.' '{print $1}'`
 do
-        for i in `tmux ls | awk -F':' '{print $1}'`
-	do
-		echo $todo_word $i
-		if [[ $i == $todo_word ]]
-		then
-			echo "YeaH Session for $todo_word already exists"
+  for i in `tmux ls | awk -F':' '{print $1}'`
+  do
+  	echo $todo_word $i
+  	if [[ $i == $todo_word ]]
+  	then
+  		echo "YeaH Session for $todo_word already exists"
                         exists=1
-		else
+  	else
                         echo "Creating Session for $todo_word"
                         new=1
-		fi
-	done
-        if [[ exists -eq 0 ]] 
-	then 
-                tmux new -s $todo_word -d
-        fi
+  	fi
+  done
+  
+  if [[ exists -eq 0 ]] 
+  then 
+    tmux new -s $todo_word -d
+  fi
+
 done
